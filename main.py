75ff3e102db485b6c4ccf549c30a58c53b33bb46
lahzi <chanathaleaphisit@gmail.com>
2022-08-25 10:41:04 +0200

    [TASK] add rows append

--- main.py
+++ main.py
@@ -7,12 +7,20 @@ from os.path import isfile, join
 currentDirectoryPath = './current/'
 currentFileName = 'data.json'
 
-def saveJSON () :
-    if os.path.exists(currentDirectoryPath + currentFileName):
-        os.remove(currentDirectoryPath + currentFileName)
+
+def createRow (columnHeaders) :
+    data = []
+    amountOfColumn = len(columnHeaders)
+
+    for column in columnHeaders :
+        value = input("Bitte geben Sie den Wert für die Spalte " + column + " ein: ")
+        data.append(value)
+
+    return data
 
 def importCSV () :
     filePath = './import/pending/'
+    fileSavePath = './import/processed/'
     files = [f for f in listdir(filePath) if isfile(join(filePath, f))]
 
     for file in files:
@@ -20,30 +28,33 @@ def importCSV () :
             reader = csv.reader(f)
             data = list(reader)
             headers = data[0][0].split(';')
-            jsonList = []
-            index = 0
+            mode = 1
 
-            for row in data:
-                if index > 0:
-                    fields = row[0].split(';')
-                    i = 0
-                    entry = []
-                    for header in headers:
-                        entry.append([
-                            header,
-                            fields[i]
-                        ])
-                        i = i + 1
+            while mode != "0":
+                print("aktuelle Datei: " + file)
+                print("Menü:")
+                print("1: Zeile hinzufügen")
+                print("0: Speichern und beenden")
 
-                    jsonList.append(entry)
-                index = index + 1
+                mode = input("Was möchten Sie tun ? ")
 
-            jsonString = json.dumps(jsonList, indent=4, sort_keys=True)
-            print(jsonString)
-            f = open(currentDirectoryPath + file + '.json', "w")
-            f.write(jsonString)
-            f.close()
+                if mode == "1" :
+                    newData = createRow(headers)
+                    data.append(newData)
+
+                if mode == "0" :
+                    saveF = open(fileSavePath + file, 'w')
+                    writer = csv.writer(saveF, delimiter=';')
+                    for row in data :
+                        writer.writerow(row)
+                        saveF.flush()
+
+                    saveF.flush()
+                    saveF.close()
+                    os.remove(filePath + file)
+
+
+
 
 if __name__ == '__main__':
     importCSV()
-    saveJSON()
